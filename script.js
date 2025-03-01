3df517a9f28c5e21d773266d4984c86e590635f2
Letícia Garcez <lpgarcez@protonmail.com>
2025-03-01 10:18:41 -0300

    adiciona carregamento

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 (function () {
-    // API: https://www.frankfurter.app/
-    const spanDolar = document.getElementById("dolar")
+	// API: https://www.frankfurter.app/
+	const texto = document.getElementById("texto");
 
-    const url = "https://api.frankfurter.app/latest?amount=1&from=USD&to=BRL";
-    fetch(url)
-        .then((resposta) => resposta.json())
-        .then((dados) => {
-            console.log(dados)
-            const valorDolar = dados.rates.BRL;
-            spanDolar.innerText = `R$ ${valorDolar.toFixed(2)}`;
-        });
+	const url = "https://api.frankfurter.app/latest?amount=1&from=USD&to=BRL";
+	fetch(url)
+		.then((resposta) => resposta.json())
+		.then((dados) => {
+			console.log(dados);
+			const valorDolar = dados.rates.BRL;
+			texto.innerText = `E ai carinha que mora logo ali, passa R$ ${valorDolar.toFixed(
+				2
+			)}.`;
+		});
 })();
